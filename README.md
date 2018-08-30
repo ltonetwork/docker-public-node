ca703017924a6617abb43a2fa47ec2f52ab80cdb
sven4ask <sven@legalthings.io>
2018-08-30 22:23:02 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# LTO Public Node in Docker
+
+## Running the image
+
+The simplest way to run a container:
+```
+docker run -it legalthings/public-node
+```
+
+**For MAINNET:**
+ 
+Mainnet will be released soon.
+
+**For TESTNET:**
+```
+docker run -p 6869:6869 -p 6863:6863 -e LTO_LOG_LEVEL=DEBUG -e LTO_HEAP_SIZE=2g -v YOUR_LOCAL_PATH_HERE:/lto legalthings/public-node    
+``` 
+
+**You can run container with predefined environment variables:**
+
+|Env variable                 |Description   |
+|-----------------------------|--------------|
+|`LTO_WALLET_SEED`          |Plain text seed for node wallet. Container converts it to base58.   |
+|`LTO_WALLET_SEED_BASE58`   |Base58 encoded seed. (Supersedes LTO_WALLET_SEED)  |
+|`LTO_WALLET_PASSWORD`      |Password for wallet file.    |
+|`LTO_API_KEY`              |ApiKey used for the rest api authentication |
+|`LTO_NETWORK`              |Available values are `MAINNET`, `TESTNET` and `CUSTOM`.   |
+|`LTO_LOG_LEVEL`            |Node logging level, available values: `OFF`, `ERROR`, `WARN`, `INFO`, `DEBUG`, `TRACE`. |
+|`LTO_HEAP_SIZE`            |Java Heap Size limit in -X Command-line Options notation (`-Xms=[your value]`). More details [here](https://docs.oracle.com/cd/E13150_01/jrockit_jvm/jrockit/jrdocs/refman/optionX.html)   |
+|`LTO_CONFIG_FILE`          |Path to your LTO Configuration file.   |
+|`LTO_DECLARED_ADDRESS`     |String with IP address and port to send as external address during handshake. Could be set automatically if UPnP is enabled. If `declared-address` is set, which is the common scenario for nodes running in the cloud, the node will just listen to incoming connections on `bind-address:port` and broadcast its `declared-address` to its peers.|
+
+**Note: All variables are optional.**  
---
+++ starter.py
@@ -0,0 +1,106 @@
+import os
+import os.path
+import urllib.request
+import sys
+import requests
+from pyhocon import ConfigFactory, HOCONConverter
+import pywaves as pw
+import base58
+import string
+import random
+import math
+from tqdm import tqdm
+from shutil import copyfile
+from pyblake2 import blake2b
+from hashlib import sha256
+
+network_names = ['MAINNET', 'TESTNET']
+
+NETWORK = os.environ.get('LTO_NETWORK')
+
+
+def generate_password(size=12, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for i in range(size))
+
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+
+def create_configs_dir():
+    if not os.path.isdir("/lto/configs"):
+        os.mkdir("/lto/configs")
+    if not os.path.isdir("/lto/data"):
+        os.mkdir("/lto/data")
+
+
+def parse_env_variables():
+    dictionary = dict()
+    for env_key in os.environ:
+        if "__" in env_key:
+            parts = env_key.split('__')
+            keys = [x.lower().replace('_', '-') for x in parts]
+            nested_set(dictionary, keys, os.environ[env_key])
+    return dictionary
+
+
+def get_wallet_data():
+    seed = os.environ.get('LTO_WALLET_SEED', pw.Address().seed)
+    seed_base58 = os.environ.get('LTO_WALLET_SEED_BASE58')
+    base58_provided = False
+    if seed_base58 is not None:
+        try:
+            base58.b58decode_check(seed_base58)
+            base58_provided = True
+        except:
+            seed_base58 = base58.b58encode(seed.encode())
+    else:
+        seed_base58 = base58.b58encode(seed.encode())
+    password = os.environ.get('LTO_WALLET_PASSWORD', generate_password())
+    if base58_provided is False:
+        print('Seed phrase:', seed)
+    print('Wallet password:', password)
+    return seed_base58, password
+
+def secureHash(message):
+    h = blake2b(digest_size=32)
+    h.update(message.encode())
+    return base58.b58encode(sha256(h.digest()).digest())
+
+if __name__ == "__main__":
+    if NETWORK is None or NETWORK not in network_names:
+        NETWORK = 'TESTNET'
+
+    create_configs_dir()
+
+    if NETWORK == 'MAINNET':
+        copyfile('/lto-node/lto-mainnet.conf', '/lto/configs/lto-config.conf')
+    elif NETWORK == 'TESTNET':
+        copyfile('/lto-node/lto-testnet.conf', '/lto/configs/lto-config.conf')
+
+    if os.path.isfile('/lto/configs/local.conf'):
+        print('config file already created')
+        sys.exit(0)
+
+    api_key = os.environ.get('LTO_API_KEY', 'lt1secretapikey!')
+    api_key_hash = secureHash(api_key)
+
+    env_dict = parse_env_variables()
+    lto_data = get_wallet_data()
+
+    nested_set(env_dict, ['lto', 'directory'], '/lto')
+    nested_set(env_dict, ['lto', 'data-directory'], '/lto/data')
+    nested_set(env_dict, ['lto', 'wallet', 'seed'], lto_data[0])
+    nested_set(env_dict, ['lto', 'wallet', 'password'], lto_data[1])
+    nested_set(env_dict, ['lto', 'rest-api', 'api-key-hash'], api_key_hash)
+
+    LTO_DECLARED_ADDRESS = os.getenv('LTO_DECLARED_ADDRESS')
+    if LTO_DECLARED_ADDRESS is not None:
+        nested_set(env_dict, ['lto', 'network', 'declared-address'], LTO_DECLARED_ADDRESS)
+
+    config = ConfigFactory.from_dict(env_dict)
+    local_conf = HOCONConverter.convert(config, 'hocon')
+    with open('/lto/configs/local.conf', 'w') as file:
+        file.write(local_conf)

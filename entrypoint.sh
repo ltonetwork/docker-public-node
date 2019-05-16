b14691c19f33b59d6516d41ec2621028f0c179db
Stefan van Oirschot <sot015@users.noreply.github.com>
2019-05-16 18:20:06 +0200

    Update entrypoint.sh
    When using Red Hat Enterprise Linux 7 Universal Base Image a specification of the Python path is not required.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/python3 "/lto-node/starter.py"
+python3 "/lto-node/starter.py"
 echo "Node is starting..."
 /usr/bin/java -Dlogback.stdout.level="${LTO_LOG_LEVEL}" "-Xmx${LTO_HEAP_SIZE}" -jar "/lto-node/lto-public-all.jar" $LTO_CONFIG_FILE

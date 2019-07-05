b3e18592e94beb8275aaa501fe5cd2d47a71b96f
sven4ask <sven@legalthings.io>
2019-07-05 15:56:13 +0200

    Fix java path in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/python3 "/lto-node/starter.py"
 echo "Node is starting..."
-/usr/bin/java -Dlogback.stdout.level="${LTO_LOG_LEVEL}" "-Xmx${LTO_HEAP_SIZE}" -jar "/lto-node/lto-public-all.jar" $LTO_CONFIG_FILE
+${JAVA_HOME}/bin/java -Dlogback.stdout.level="${LTO_LOG_LEVEL}" "-Xmx${LTO_HEAP_SIZE}" -jar "/lto-node/lto-public-all.jar" $LTO_CONFIG_FILE
